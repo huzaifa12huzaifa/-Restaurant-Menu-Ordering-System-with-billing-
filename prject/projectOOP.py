191a01488dcd65a327867c289dce8b5c8ca74fd6
huzaifa12huzaifa <139149177+huzaifa12huzaifa@users.noreply.github.com>
2023-07-16 11:24:33 +0500

    Update projectOOP.py

--- prject/projectOOP.py
+++ prject/projectOOP.py
@@ -1,10 +1,19 @@
 class Name:
     def __init__(self,name):
-        name = name
+        self.name = name
     def print_name(self):
-        return f">>>>>>>>>>>> Welcome To <<<<<<<<<<<<<<\n{name}"
-name= "****************************** \n*   Eats & Meets Restaurent  *\n******************************"
-restaurent_name=Name(name)
-print(restaurent_name.print_name())
+        return f">>>>>>>>>>>> Welcome To <<<<<<<<<<<<<<\n{self.name}"
+name= "****************************** \n*       Taste Junction       *\n******************************"
+restaurent_name =Name(name)
 
 
+
+class Menu(Name):
+    def __init__(self,name,menu):
+        Name.__init__(self,name)
+        self.menu = menu
+    def starter(self):
+        pass
+menu= "Menu of the restaurent"
+ob=Menu(name,menu)
+print(ob.print_name())

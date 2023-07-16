0f195b6da06646311326b27ba99b734430dbefd2
huzaifa12huzaifa <139149177+huzaifa12huzaifa@users.noreply.github.com>
2023-07-16 11:10:45 +0500

    Update projectOOP.py

--- prject/projectOOP.py
+++ prject/projectOOP.py
@@ -1,5 +1,10 @@
+class Name:
+    def __init__(self,name):
+        name = name
+    def print_name(self):
+        return f">>>>>>>>>>>> Welcome To <<<<<<<<<<<<<<\n{name}"
+name= "****************************** \n*   Eats & Meets Restaurent  *\n******************************"
+restaurent_name=Name(name)
+print(restaurent_name.print_name())
 
 
-
-for x in range(1,10):
-  print("zarar ka lun chota ha")
